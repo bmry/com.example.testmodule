95f35aa97d357148e6f4570ab5cc199d5a8debf0
bmry <bamgbosemorayo@gmail.com>
2016-09-13 12:49:11 +0100

    Contact Extension Tab

---
+++ ang/testmodule.ang.php
@@ -0,0 +1,24 @@
+<?php
+// This file declares an Angular module which can be autoloaded
+// in CiviCRM. See also:
+// http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+
+return array (
+  'js' => 
+  array (
+    0 => 'ang/testmodule.js',
+    1 => 'ang/testmodule/*.js',
+    2 => 'ang/testmodule/*/*.js',
+  ),
+  'css' => 
+  array (
+    0 => 'ang/testmodule.css',
+  ),
+  'partials' => 
+  array (
+    0 => 'ang/testmodule',
+  ),
+  'settings' => 
+  array (
+  ),
+);
